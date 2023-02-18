0ad634c7e9c6722a86ccab22302f56d84a1ea99f
Mateusz Łoskot <mateusz@loskot.net>
2023-02-18 17:37:40 +0100

    sed

--- terraform-stack-pipeline-pr-comment.sh
+++ terraform-stack-pipeline-pr-comment.sh
@@ -25,9 +25,8 @@ build_number="${3}"
 logs_collected=0
 
 echo -e "\033[32;1mINFO:\033[0m Rendering Terraform ${command} comment from ${logs_path}"
-output="## Build ${build_number}: Terraform ${command}\n\n"
-output=""
-
+comment="## Build ${build_number}: Terraform ${command}\n\n"
+comment="<verbatim>"
 for cmd_log in "${logs_path}"/*".${command}.txt"; do
     echo -e "\033[32;1mINFO:\033[0m Reading ${command} output from ${cmd_log}"
     # Extract name of known layer from e.g. dev_04-platform.validate.txt
@@ -35,33 +34,33 @@ for cmd_log in "${logs_path}"/*".${command}.txt"; do
     layer=$(echo "${layer}" | cut -d '_' -f 2 | cut -d . -f 1)
     raw_log=$(< "${cmd_log}")
     # Render section for layer
-    #output+="### Layer: ${layer}\n\n"
-    output+="${raw_log}\n"
+    #comment+="### Layer: ${layer}\n\n"
+    comment+=$(echo "${raw_log}" | iconv -c -f utf-8 -t ascii | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
     # shellcheck disable=SC2076
     # if [[ "${raw_log}" =~ "Success! The configuration is valid." ]]; then
-    #     output+="${raw_log}\n"
+    #     comment+="${raw_log}\n"
     # else
-    #     output+="<details><summary>Show</summary>\n\n<verbatim>\n${raw_log}\n</verbatim>\n</details>\n\n"
+    #     comment+="<details><summary>Show</summary>\n\n<verbatim>\n${raw_log}\n</verbatim>\n</details>\n\n"
     # fi
     ((logs_collected++))
     if [[ $logs_collected -gt 1 ]]; then
         break
     fi
 done
+comment+="</verbatim>"
 
 echo -e "\033[32;1mINFO:\033[0m Exporting TERRAFORM_COMMAND_PR_COMMENT environment variable"
 if [[ $logs_collected -gt 0 ]]; then
-    #RESULT_PR_COMMENT=$(echo -e "${output}")
-    RESULT_PR_COMMENT="${output}"
+    TERRAFORM_COMMAND_PR_COMMENT="${comment}"
 else
-    RESULT_PR_COMMENT=""
+    TERRAFORM_COMMAND_PR_COMMENT=""
 fi
 
-if command -v "iconv" &> /dev/null; then
-    echo -e "\033[32;1mINFO:\033[0m Running iconv to escape comment content"
-    TERRAFORM_COMMAND_PR_COMMENT=$(echo "$RESULT_PR_COMMENT" | iconv -c -f utf-8 -t ascii | sed 's/$/\\n/' | tr -d '\n')
-else
-    echo -e "\033[32;1mINFO:\033[0m iconv not found, escaping comment content using simpler methods"
-fi
+# if command -v "iconv" &> /dev/null; then
+#     echo -e "\033[32;1mINFO:\033[0m Running iconv to escape comment content"
+#     TERRAFORM_COMMAND_PR_COMMENT=$(echo "$RESULT_PR_COMMENT" | iconv -c -f utf-8 -t ascii | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
+# else
+#     echo -e "\033[32;1mINFO:\033[0m iconv not found, escaping comment content using simpler methods"
+# fi
 
 export TERRAFORM_COMMAND_PR_COMMENT
