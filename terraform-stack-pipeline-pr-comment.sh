923cf889db7f414bf8ef77e58be95b966f393dfc
Mateusz Łoskot <mateusz@loskot.net>
2023-02-18 16:00:03 +0100

    rawurlencode

--- terraform-stack-pipeline-pr-comment.sh
+++ terraform-stack-pipeline-pr-comment.sh
@@ -55,9 +55,28 @@ else
     RESULT_PR_COMMENT=""
 fi
 
+rawurlencode() {
+    local string="${1}"
+    local strlen=${#string}
+    local encoded=""
+    local pos c o
+
+    for (( pos=0 ; pos<strlen ; pos++ )); do
+        c=${string:$pos:1}
+        case "$c" in
+           [-_.~a-zA-Z0-9] ) o="${c}" ;;
+           * )               printf -v o '%%%02x' "'$c"
+        esac
+        encoded+="${o}"
+    done
+    echo "${encoded}"    # You can either set a return variable (FASTER) 
+    REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+}
+
 if command -v "uni2ascii" &> /dev/null; then
     echo -e "\033[32;1mINFO:\033[0m Running uni2ascii to escape comment content"
-    TERRAFORM_COMMAND_PR_COMMENT=$(printf %s "$RESULT_PR_COMMENT" | uni2ascii -q -d)
+    #TERRAFORM_COMMAND_PR_COMMENT=$(printf %s "$RESULT_PR_COMMENT" | uni2ascii -q -d)
+    TERRAFORM_COMMAND_PR_COMMENT=$( rawurlencode "$RESULT_PR_COMMENT" )
 else
     echo -e "\033[32;1mINFO:\033[0m uni2ascii not found, escaping comment content using simpler methods"
 fi
