7678f746f7360e543b72ac2633650e3654df480b
Mateusz Łoskot <mateusz@loskot.net>
2023-02-18 15:48:31 +0100

    colors

--- terraform-stack-pipeline-pr-comment.sh
+++ terraform-stack-pipeline-pr-comment.sh
@@ -2,16 +2,20 @@
 # Collect outputs of Terraform command run for the (ordered) known layers
 # and generate content of PR comment in Markdown,
 # and return it via environment variable.
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 <terraform command> <path to terraform command output files>"
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 <terraform command> <path to terraform command output files> <build number>"
     exit 1
 fi
 if [[ -z "$1" ]]; then
-    echo "Missing terraform command"
+    echo -e "\033[31;1mERROR:\033[0m Missing terraform command."
     exit 1
 fi
 if [[ ! -d "$2" ]]; then
-    echo "Missing path to terraform command output files"
+    echo -e "\033[31;1mERROR:\033[0m Missing path to terraform command output files."
+    exit 1
+fi
+if [[ -z "$3" ]]; then
+    echo -e "\033[31;1mERROR:\033[0m Missing build number."
     exit 1
 fi
 
@@ -19,11 +23,11 @@ command="${1}"
 logs_path="${2}"
 logs_collected=0
 
-echo -e "\033[34;1mINFO:\033[0m Rendering Terraform ${command} comment from ${logs_path}"
+echo -e "\033[32;1mINFO:\033[0m Rendering Terraform ${command} comment from ${logs_path}"
 output="## Build ${BUILD_BUILDNUMBER}: Terraform ${command}\n\n"
 
 for cmd_log in "${logs_path}"/*".${command}.txt"; do
-    echo -e "\033[34;1mINFO:\033[0m Reading ${command} output from ${cmd_log}"
+    echo -e "\033[32;1mINFO:\033[0m Reading ${command} output from ${cmd_log}"
     # Extract name of known layer from e.g. dev_04-platform.validate.txt
     layer=$(basename "${cmd_log}")
     layer=$(echo "${layer}" | cut -d '_' -f 2 | cut -d . -f 1)
@@ -42,7 +46,7 @@ for cmd_log in "${logs_path}"/*".${command}.txt"; do
     fi
 done
 
-echo -e "\033[34;1mINFO:\033[0m Exporting TERRAFORM_COMMAND_PR_COMMENT environment variable"
+echo -e "\033[32;1mINFO:\033[0m Exporting TERRAFORM_COMMAND_PR_COMMENT environment variable"
 if [[ $logs_collected -gt 0 ]]; then
     #RESULT_PR_COMMENT=$(echo -e "${output}")
     RESULT_PR_COMMENT="${output}"
@@ -51,10 +55,10 @@ else
 fi
 
 if command -v "uni2ascii" &> /dev/null; then
-    echo -e "\033[34;1mINFO:\033[0m Running uni2ascii to escape comment content"
+    echo -e "\033[32;1mINFO:\033[0m Running uni2ascii to escape comment content"
     TERRAFORM_COMMAND_PR_COMMENT=$(printf %s "$RESULT_PR_COMMENT" | uni2ascii -q -a J)
 else
-    echo -e "\033[34;1mINFO:\033[0m uni2ascii not found, escaping comment content using simpler methods"
+    echo -e "\033[32;1mINFO:\033[0m uni2ascii not found, escaping comment content using simpler methods"
 fi
 
 export TERRAFORM_COMMAND_PR_COMMENT
