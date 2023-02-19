dd2469c8d559a8b908fa7ecba70e1a92a93415a5
Mateusz Łoskot <mateusz@loskot.net>
2023-02-19 02:29:26 +0100

    Remove auxiliary script

--- dump-plan-json-summary.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-changes=$(cat "${1}" | jq -r '[.resource_changes[]? | { resource: .address, action: .change.actions[] } | select (.action != "no-op")]')
-summary=$(echo $changes | jq -r '.   | "Environment has \(length) changes"')
-details=$(echo $changes | jq -r '.[] | "* \(.resource) will be \(.action)d"')
-echo "Summary: ${summary}"
-echo "${details}"
